4df537d7dbd70b384fb7b97a48387409b726517e
Brenda Schmidt <musicchick5285@yahoo.com>
2016-02-19 10:31:30 -0500

    Create to do items complete

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
+gem 'bootstrap-sass', '~> 3.3.6'
+
 group :development, :test do
   gem 'rspec-rails', '~> 3.4'
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+
+  def create
+    @item = Item.new(params[:item].permit!)
+    @item.user = current_user
+    @item.save
+    redirect_to :back
+  end
+
 end
